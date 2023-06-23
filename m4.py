c0eedee94aff8b7ae2484cfb64335c2f1adacc8f
Phil Parisi <philsbeginnercode@gmail.com>
2023-06-23 04:17:56 -0400

    fresh update to test branch

--- m4.py
+++ m4.py
@@ -10,9 +10,8 @@ def search_comports(serial_number):
     for port in serial.tools.list_ports.comports():
         
         if type(port.serial_number) != str:
-
             continue
-
+        
         if port.serial_number == serial_number:
             return port
     
@@ -24,15 +23,15 @@ class M4:
     
         self.page_size = page_size
                 
-        
         self.port = search_comports(serial_number)
-       
+        
         self.bat_log = bat_log
                     
-        if str(self.port) == "None":
+        if self.port == "None":
             self.bat_log.critical(f"Could not find device with serial number {serial_number}")
             exit()
             
+        
         self.sercom = serial.Serial(self.port.device)
             
         self.bat_log.info(f"Found device {serial_number} on {self.port.device}")
