6821d627d5ce4dc2abc08374ff3377093e02de65
Phil Parisi <philsbeginnercode@gmail.com>
2023-08-03 14:22:19 +0800

    updated GPS logger

--- rtk-gps/IRES_GPSlogger.py
+++ rtk-gps/IRES_GPSlogger.py
@@ -7,6 +7,22 @@ import datetime
 import serial
 import csv
 import os
+import sys
+import signal
+import time
+
+
+# signal handling function for shutting down the process
+def signal_handler(sig, frame):
+    # close the csv file before exiting
+    file.close()
+    print("Logging stopped by user!")
+    print("recorded " + str(num_gps_pts) + " GPS points") 
+    print("failed to get GPS " + str(gps_fails) + " times") 
+    sys.exit(0)
+
+# stages the shutting down process
+signal.signal(signal.SIGTERM, signal_handler)
 
 
 # connect via serial port (if you are on a PC, you can check this in device manager)
@@ -27,9 +43,17 @@ formatted_datetime = current_datetime.strftime("%Y%m%d%H%M%S")
 
 # create csv_filename and output to user
 csv_filename = directory_path + "/logged_gps_" + formatted_datetime + ".csv"
-print("Saving csv as " + csv_filename + " in the current directory")
-print("logging data... use 'ctrl+c' to stop")
+print('******** READ BELOW! ********')
+print("1. saving csv as " + csv_filename + " in the current directory")
+print("2. use 'ctrl+c' to stop execution of this script")
+print("3. when GPS data is logged to the csv, the coordinates are outputted below")
+print("4. if you don't see coordinates below --> NO data for you! something is WRONG!")
+print('*****************************')
+time.sleep(2)
 
+# define counter for number of samples recorded
+num_gps_pts = 0
+gps_fails = 0
 
 # open csv in append mode ('a')
 with open(csv_filename, 'a', newline='') as file:
@@ -53,12 +77,25 @@ with open(csv_filename, 'a', newline='') as file:
 				data = ser.readline().decode().rstrip()
 	
 				# print data as an output (for debugging... comment out later)
-				#print(f"Received data: {data}")
+				print(f"received data: {data}")
 
 				# write the data into a new line of the csv
 				writer.writerow(data.split(","))
 
+				# increment GPS counter
+				num_gps_pts = num_gps_pts + 1
+
+			else:
+				print('no GPS data, trying again')
+				gps_fails = gps_fails + 1
+				time.sleep(1)
+
+
+
 	except KeyboardInterrupt:
-		print("Logging stopped by user!")
+		print("\n*****************************")
+		print("logging stopped by user!")
+		print("recorded " + str(num_gps_pts) + " GPS points")
+		print("failed to get GPS " + str(gps_fails) + " times")
 
 ser.close()
